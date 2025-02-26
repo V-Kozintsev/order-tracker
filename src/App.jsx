81f56ae0b34784d586d9991f8798dd3ec85d1eb5
Viktor <v_kozintcev@mail.ru>
2025-02-26 21:46:58 +0300

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // src/components/OrderStatus.jsx
 
 import React from "react";
+import { OrderStatus } from "./components/OrderStatus";
 
 export function App() {
   return (
     <div>
-      <h1>Привет, мир!</h1>
+      <h1>Order Tracker</h1>
+      <OrderStatus />
     </div>
   );
 }
