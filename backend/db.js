d003cc74d3e33be6eaa86756197bce7bacf1dc31
Viktor <v_kozintcev@mail.ru>
2025-02-26 19:06:12 +0300

    connection to the database

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,13 @@
 //db.js # Файл для подключения к базе данных
+const { Pool } = require("pg");
+require("dotenv").config(); // Загружаем переменные окружения из .env
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: String(process.env.DB_PASSWORD),
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
+
+module.exports = pool;
