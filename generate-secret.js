2ee553efa2a78d97a7c1223af3b75ea612e9b104
Viktor <v_kozintcev@mail.ru>
2025-03-16 21:04:51 +0300

    бновлены зависимости и пересобраны Docker-образы

--- generate-secret.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-function generateSecureToken(length = 64) {
-  return crypto.randomBytes(length).toString("hex");
-}
-
-const token = generateSecureToken();
-console.log("Сгенерированный токен:", token);
